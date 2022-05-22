3ba254a342803957ecaf345e55d42bb0fdb5f61f
Jetch123 <67650698+Jetch123@users.noreply.github.com>
2022-05-22 16:38:44 +0200

    Add files via upload

---
+++ views.sql
@@ -0,0 +1,37 @@
+DROP VIEW IF EXISTS numberofcars;
+CREATE VIEW numberofcars AS (
+	SELECT 	store.storeid as 'Store ID',
+			store.street as 'Street',
+			store.City as 'City',
+			COUNT(vehicle.storeid) as 'Number of Vehicles'
+	FROM Store
+	LEFT JOIN vehicle ON store.storeid=vehicle.storeid
+	GROUP BY store.storeid
+	ORDER BY COUNT(vehicle.storeid) DESC
+	
+);	
+
+
+CREATE VIEW properemployee AS (
+	SELECT
+	firstname as "Name",
+	lastname as "Surname",
+	IRS_NUMBER AS "IRS Number",
+	driverlicense as "Driver's License",
+	socialsecuritynumber as "Social Security Number",
+	STREET AS "Street",
+	streetnumber as "Number",
+	City as "City",
+	Postalcode as "ZIP"
+	FROM EMPLOYEE);
+	
+CREATE VIEW horsepower AS (
+	SELECT
+	HorsePower as "Horse Power(HP)",
+	Model as "Model",
+	Make as "Maker",
+	cylindercapacity as "Cylinder Capacity (cc)"
+	FROM vehicle
+	ORDER BY horsepower DESC);
+
+	
